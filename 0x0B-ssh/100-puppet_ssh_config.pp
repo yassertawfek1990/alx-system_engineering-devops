55b1a151b6587eba737d3f629b60fe1d8aa11b51
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-24 14:07:23 +0200

    c

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Puppet
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
