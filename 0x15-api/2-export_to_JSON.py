96db3e7a5962149e8d0ee5940924e4aa05a5523d
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-03-26 00:15:30 +0200

    c

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
 """Ext."""
-import csv
 import json
 import requests
-import sys
 
+if __name__ == "__main__":
+    urla = "https://jsonplaceholder.typicode.com/"
+    s = requests.get(urla + "users").json()
 
-if __name__ == '__main__':
-    d = sys.argv[1]
-    la = 'https://jsonplaceholder.typicode.com/users/' + d
-    r = requests.get(la)
-    """Documentation"""
-    n = r.json().get('username')
-    """Documentation"""
-    lt = la + '/todos'
-    r = requests.get(lt)
-    t = r.json()
-    di = {d: []}
-    for x in t:
-        c = t.get('completed')
-        h = t.get('title')
-        di[d].append({
-                                  "task": h,
-                                  "completed": c,
-                                  "username": n})
-    """print(dict_data)"""
-    with open('{}.json'.format(d), 'w') as z:
-        json.dump(di, z)
+    with open("todo_all_employees.json", "w") as j:
+        json.dump({
+            x.get("id"): [{
+                "task": c.get("title"),
+                "completed": c.get("completed"),
+                "username": x.get("username")
+            } for c in requests.get(urla + "todos",
+                                    params={"userId": x.get("id")}).json()]
+            for x in s}, j)
