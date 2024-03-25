025aba5d08202094fd1e5de482282e1a342fc790
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-03-25 23:58:14 +0200

    c

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Ext."""
+import json
+import requests
+
+if __name__ == "__main__":
+    la = "https://jsonplaceholder.typicode.com/"
+    u = requests.get(la + "users").json()
+
+    with open("todo_all_employees.json", "w") as j:
+        json.dump({
+            x.get("id"): [{
+                "task": c.get("title"),
+                "completed": c.get("completed"),
+                "username": c.get("username")
+            } for c in requests.get(la + "todos",
+                                    params={"userId": x.get("id")}).json()]
+            for x in u}, j)
