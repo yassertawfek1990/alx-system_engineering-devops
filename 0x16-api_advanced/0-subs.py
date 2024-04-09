5aedc9ca2b06db950bffda1723fcc30c652e2a03
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-04-09 20:56:19 +0200

    c

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """R"""
     u = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    h = {
+    headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    r = requests.get(u, headers=h, allow_redirects=False)
+    r = requests.get(u, headers=headers, allow_redirects=False)
     if r.status_code == 404:
         return 0
     s = r.json().get("data")
