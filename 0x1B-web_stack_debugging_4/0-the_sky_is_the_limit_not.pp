cd131f8112267ac59285bcfd9e68c05d262d9a45
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-04-18 23:33:04 +0200

    c

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-# Increahandle.
+# Increases server can handle.
 
-# Incle
+# Increase default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
